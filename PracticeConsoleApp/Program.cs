def1829afdd4c0ce7e41b12c8401a7672b856ef9
Nelso Villalobos <nelso.lobos13@gmail.com>
2023-01-26 22:44:00 -0500

    Some extra changes

--- PracticeConsoleApp/Program.cs
+++ PracticeConsoleApp/Program.cs
@@ -11,11 +11,14 @@ Console.WriteLine(dog);
 Console.WriteLine(dog.getAge() + "  " + dog.getType() + "  wIs it a pet?: " + dog.getIsPet()); //Practicing with C#
 Console.WriteLine(dog.GetType()); //Gives class type
 
+Console.WriteLine("\n\\\\\\\\Just added this line to practice some things with git////");
+
 /*Trying to get better at Git so i am writing this to add it and then commit it. I will then push this onto the remote
  project on github*/
 
 //Created a remote project and connected this local git to that one.
 //Added this into the new branch called newBranch
+//still practicing more with git
 class Animal
 {
     private string _type;
